50993f831115c145be12446549181c9e72d14893
Gabriel Gilder <gabriel@gabrielgilder.com>
2022-07-11 14:31:36 -0700

    Catch errors in execution and clean up repo

--- backup_icloud_notes.rb
+++ backup_icloud_notes.rb
@@ -114,80 +114,85 @@ if git_repo_dirty(git_repo)
   exit(1)
 end
 
-# Delete existing notes so deletions will be caught
-File.delete(*Dir[File.join(backup_destination, "**", "*.html")])
+begin
+  # Delete existing notes so deletions will be caught
+  File.delete(*Dir[File.join(backup_destination, "**", "*.html")])
 
+  folder_count = get_count("folders")
+  folder_names = (1..folder_count).map { |n| get_text("name of folder #{n}") }
+  folder_names.reject! { |name| name == "Recently Deleted" }
 
-folder_count = get_count("folders")
-folder_names = (1..folder_count).map { |n| get_text("name of folder #{n}") }
-folder_names.reject! { |name| name == "Recently Deleted" }
+  notes_count = 0
+  notes_with_attachments = []
+  puts "Backing up #{folder_names.count} folders"
+  folder_names.each do |folder_name|
+    notes_count = get_count(%{notes in folder "#{folder_name}"})
+    puts %{Backing up #{notes_count} notes in folder "#{folder_name}"}
+    FileUtils.mkdir_p(File.join(backup_destination, folder_name))
+    (1..notes_count).each do |note_idx|
+      note_query = %{note #{note_idx} of folder "#{folder_name}"}
+      note_name = get_text(%{name of #{note_query}})
+      creation_date = get_date(%{creation date of #{note_query}})
+      mod_date = get_date(%{modification date of #{note_query}})
+      note_file_name = mod_date.strftime('%Y-%m-%d ') + note_name.gsub(%r([/:]), "-") + ".html"
+      note_path = File.join(backup_destination, folder_name, note_file_name)
+      if File.exist?(note_path)
+        raise %{File name collision: "#{note_path}" already exists!}
+      end
 
-notes_count = 0
-notes_with_attachments = []
-puts "Backing up #{folder_names.count} folders"
-folder_names.each do |folder_name|
-  notes_count = get_count(%{notes in folder "#{folder_name}"})
-  puts %{Backing up #{notes_count} notes in folder "#{folder_name}"}
-  FileUtils.mkdir_p(File.join(backup_destination, folder_name))
-  (1..notes_count).each do |note_idx|
-    note_query = %{note #{note_idx} of folder "#{folder_name}"}
-    note_name = get_text(%{name of #{note_query}})
-    creation_date = get_date(%{creation date of #{note_query}})
-    mod_date = get_date(%{modification date of #{note_query}})
-    note_file_name = mod_date.strftime('%Y-%m-%d ') + note_name.gsub(%r([/:]), "-") + ".html"
-    note_path = File.join(backup_destination, folder_name, note_file_name)
-    if File.exist?(note_path)
-      $stderr.puts %{File name collision: "#{note_path}" already exists!}
-      exit 1
-    end
+      html = rtf_to_html(get_note_rtf(note_query))
+      html.sub!(/<body>/, "<body>\n<p>Created: #{creation_date}<br>Modified: #{mod_date}</p>")
 
-    html = rtf_to_html(get_note_rtf(note_query))
-    html.sub!(/<body>/, "<body>\n<p>Created: #{creation_date}<br>Modified: #{mod_date}</p>")
+      if verbose
+        puts %{Backing up "#{note_name}" to "#{note_file_name}"}
+      end
+      File.write(note_path, html)
+      notes_count += 1
 
-    if verbose
-      puts %{Backing up "#{note_name}" to "#{note_file_name}"}
-    end
-    File.write(note_path, html)
-    notes_count += 1
-
-    attachments_count = get_count("attachments of #{note_query}")
-    if attachments_count > 0
-      $stderr.puts %{[WARNING] Note "#{note_name}" has attachments, which cannot currently be backed up!}
-      notes_with_attachments << note_name
+      attachments_count = get_count("attachments of #{note_query}")
+      if attachments_count > 0
+        $stderr.puts %{[WARNING] Note "#{note_name}" has attachments, which cannot currently be backed up!}
+        notes_with_attachments << note_name
+      end
     end
   end
-end
 
-# Work around bug with cached index in git gem
-begin
-  git_repo.lib.send(:command_lines, 'update-index', ['--refresh'])
-rescue
-  # The update-index command can fail, no big deal
-end
+  # Work around bug with cached index in git gem
+  begin
+    git_repo.lib.send(:command_lines, 'update-index', ['--refresh'])
+  rescue
+    # The update-index command can fail, no big deal
+  end
 
-puts
-puts "Status:"
-if new_git_repo(git_repo)
-  puts "(new repo, no status to show)"
-else
-  puts "New: #{git_repo.status.untracked.count}"
-  puts "Changed: #{git_repo.status.changed.count}"
-  puts "Deleted: #{git_repo.status.deleted.count}"
-end
+  puts
+  puts "Status:"
+  if new_git_repo(git_repo)
+    puts "(new repo, no status to show)"
+  else
+    puts "New: #{git_repo.status.untracked.count}"
+    puts "Changed: #{git_repo.status.changed.count}"
+    puts "Deleted: #{git_repo.status.deleted.count}"
+  end
 
-puts
-if git_repo_dirty(git_repo)
-  puts "Committing latest backup"
-  git_repo.add(all: true)
-  git_repo.commit("Backup notes")
-  puts "Pushing changes"
-  git_repo.push
-else
-  puts "No changes to commit."
-end
+  puts
+  if git_repo_dirty(git_repo)
+    puts "Committing latest backup"
+    git_repo.add(all: true)
+    git_repo.commit("Backup notes")
+    puts "Pushing changes"
+    git_repo.push
+  else
+    puts "No changes to commit."
+  end
 
-puts
-puts "Backed up #{notes_count} notes."
-if notes_with_attachments.count > 0
-  puts "#{notes_with_attachments.count} notes have attachments which cannot currently be backed up."
+  puts
+  puts "Backed up #{notes_count} notes."
+  if notes_with_attachments.count > 0
+    puts "#{notes_with_attachments.count} notes have attachments which cannot currently be backed up."
+  end
+rescue StandardError => e
+  git_repo.reset_hard
+  $stderr.puts e
+  $stderr.puts "Reset repo to clean state."
+  exit 1
 end
